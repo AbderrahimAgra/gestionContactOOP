f7670d78171996544a2d43b14544986c7b36ae65
AbderrahimAgra <abder.agra@gmail.com>
2022-04-21 01:00:38 +0000

    login & logout

--- version1.1/login.php
+++ version1.1/login.php
@@ -1,3 +1,36 @@
+
+<?php
+
+
+// var_dump(password_verify("test@user.com",'$2y$10$PyfrsAQUDbsz4'));
+//  exit();
+
+include'./Classes/user.class.php';
+
+
+$error = "";
+var_dump($_POST);
+if (isset($_POST['login'])){
+  $user = new User();
+    $user->setUsername($_POST['email']);
+    $user->setPassword($_POST['password']);
+    if($user->signIn()){
+        header("Location: contacts_liste.php");
+    }else{
+         $error = "incorect username or password ";  
+    }
+}else{
+  // session_start();
+  if(isset($_SESSION['id'])){
+    header("Location: contacts_liste.php");
+  }
+
+
+}
+
+
+?>
+
 <!-- header -->
 
     <?php include('./include/nav.inc.php') ; ?>
@@ -7,7 +40,7 @@
     <div class="authentification">
       <h2> Authenticate </h2>
 
-      <form action="signIn.php" method="POST">
+      <form action="login.php" method="POST">
 
         <div class=" row mb-3">
           <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
@@ -26,7 +59,7 @@
         </div>
       
         <div class="row mb-3 ">
-          <button type="submit" class="col-sm-4 offset-4 btn btn-primary">Sign in</button>
+          <button type="submit" name="login" class="col-sm-4 offset-4 btn btn-primary">Sign in</button>
         </div>
 
       </form>
